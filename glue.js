a21f3dfeb5114df601a06be0295902c119f729a7
Glenn Searby <glenn.searby@gmail.com>
2014-10-17 12:45:53 +0100

    Removed all the unnecessary things. The map is basically working as expected at this point, except with the heat map layer instead of the correct layer.

--- glue.js
+++ glue.js
@@ -4,242 +4,37 @@
 
 /*
  This file is where all the mess and wiring goes.
- We should aim to reduce its size.
+ We should always aim to reduce its size.
  */
-
-var projectPoint = function(x, y) {
-    var point = map.latLngToLayerPoint(new leaflet.LatLng(y, x));
-    this.stream.point(point.x, point.y);
-};
-
-var sortedByZ = function() {
-    return layers.enabled().slice(0).sort(function(a, b){
-	return a.options.zIndex - b.options.zIndex;
-    });
-};
-
-var getLayerEl = function(layerName) {
-    return d3.select("#map svg g#" + layerName);
-};
-
-var getLayerObjects = function(layerName) {
-    return getLayerEl(layerName)
-	.selectAll("path");
-};
-
-var log2 = function(n) {
-    return Math.log(n) / Math.LN2;
-};
-
-var zoomTo = function(bbox, map) {
-    var x1 = bbox[0],
-	y1 = bbox[1],
-	x2 = bbox[2],
-	y2 = bbox[3];
-
-    var boxSize = Math.max(
-	Math.abs(x1 - x2),
-	Math.abs(y1 - y2));
-
-    var newZoom = Math.round(
-	log2(360 / boxSize) + 1.5);
-    console.log("new zoom " + newZoom);
-    console.log("new bounds " + [(y1 + y2) / 2, (x1 + x2) / 2]);
-    
-    map.setView(
-	leaflet.latLng(
-	    (y1 + y2) / 2,
-	    (x1 + x2) / 2),
-	newZoom);
-};
-
-var startCoordinates = [0, 0],
-    zoom = 2,
+var startCoordinates = [55.5, 0],
+    zoom = 6,
     d3 = require("d3"),
     _ = require("lodash"),
     dialogue = require("floating-dialogue"),
     geocoder = require("leaflet-control-geocoder"),
     leaflet = require("leaflet"),
-
     body = d3.select("body"),
     mapDiv = body.append("div").attr("id", "map"),
     toolbar = body.append("div").attr("id", "toolbar"),
     errors = require("./errors.js")(body, toolbar),
-    time = require("./time-control.js")(body, toolbar, 2013, 2024, 2014),
-    loader = require("./loader.js"),
-    layers = require("./layers.js")(errors),
-    geometries = require("./geometries.js"),
-
-    title = require("./title.js")(body),
-    transform = d3.geo.transform({point: projectPoint});
-
-require("./lib/d3-plugins/geo/tile/tile.js");
-
-var map = new leaflet.Map("map", {
-    doubleClickZoom: false
-})
-	.setView(startCoordinates, zoom);
-
-/* The map will make us an svg. It will automatically sort out its bounds for us. */
-map._initPathRoot();
-
-map.addControl(new geocoder({
-    email: "research@cse.org.uk"
-}));
-
-var overlay = d3.select(map.getPanes().overlayPane)
-	.select("svg")
-	.attr("id", "overlay"),
-
+    legend = body.append("div").attr("id", "legend"),
+    map = new leaflet.Map("map", {
+	doubleClickZoom: false
+    })
+	.setView(startCoordinates, zoom),
     tileLayers = require("./tile-layers.js")(map, errors),
-    layersControl = require("./layer-control.js")(body, toolbar, map, layers, tileLayers, zoomTo),
-    paint = require("./paint.js")(overlay, transform, sortedByZ),
-    worksheet = require("./worksheet.js")(),
-    resultsTable = require("./results-table.js"),
-    colours = require("./colour.js");
+    reverseColour = require("./colour.js").reverse;
 
-var updateResults = function(l) {
-    l.resultsTable.info(
-	l.worksheet.headers(), 
-	l.worksheet.data(),
-	l.worksheet.getSortProperties()
-    );
-};
+map
+    .addControl(new geocoder({
+	email: "research@cse.org.uk"
+    }))
+    .addLayer(tileLayers.base)
+    .addLayer(tileLayers.overlay);
 
-layers.layerCreated(function(l) {
-    var updateLResults = function() {
-	return updateResults(l);
-    };
-
-    var recolour = function() {
-	paint.redrawAll();
-
-	if (l.worksheet.getSortProperties().properties.length > 0) {
-	    var colour = l.worksheet.colour(),
-		col = l.worksheet.firstSortPropertyI();
-
-	    l.resultsTable.rows().each(function(d, i) {
-		d3.select(this)
-		    .selectAll("td")
-		    .each(function(d, i) {
-			var el = d3.select(this);
-			var background = i === col ? colour(d) : null,
-			    font = i === col ? colours.reverse(background) : null;
-
-			el
-			    .style("background-color", background)
-			    .style("color", font);
-		    });
-	    });
-	} else {
-	    l.resultsTable.cells()
-		.style("background-color", null)
-		.style("color", null);
-	}
-    };
-
-    l.worksheet = worksheet(l.geometry());
-    l.resultsTable = resultsTable(body);
-    l.resultsTable.headerClicked(function(p) {
-	l.worksheet.sortProperty(p, d3.event.shiftKey);
-    });
-    l.resultsTable.rowClicked(function(d, i) {
-	var id = d[0];
-
-	d3.select("#map svg g#" + l.name() + " path#" + id)
-	    .each(function(d, i) {
-		zoomTo(d.bbox, map);
-	    });
-
-	l.resultsTable.rows().
-	    classed("selected", function(d, i) {
-		return d[0] === id;
-	    });
-    });
-    l.resultsTable.rowHovered(function(d, i) {
-	var id = d[0];
-	
-	getLayerObjects(l.name()).classed("highlight", function(d, i) {
-	    return d.id === id;
-	});
-    });
-    l.resultsTable.resetClicked(function() {
-	l.worksheet.sortProperty();
-    });
-
-    // TODO: only redraw the layer which changed.
-    l.worksheet.baseColourChanged(recolour);
-    l.worksheet.sortPropertyChanged(_.compose(recolour, updateLResults));
-
-    updateResults(l);
-    layersControl.update();
+tileLayers.overlay.colourChanged(function(colour) {
+    legend
+	.style("background-color", colour)
+	.style("color", reverseColour(colour))
+	.text(tileLayers.overlay.legend(colour));
 });
-layers.layerChanged(function(l) {
-    if (l) {
-	updateResults(l);
-    }
-    paint.redrawAll();
-});
-layers.layerRemoved(function(l) {
-    l.resultsTable.el().remove();
-    paint.redrawAll();
-});
-
-map.on("viewreset", paint.redrawAll);
-
-var handlers = require("./file-handlers.js")(
-    errors, 
-    geometries, 
-    layers, 
-    paint.redrawAll
-);
-require("./file-drop.js")(d3.select("body"), errors, handlers);
-
-var wikiStore = require("./wiki-store.js")(
-    errors, 
-    body,
-    toolbar,
-    map,
-    layersControl,
-    layers,
-    tileLayers,
-    title,
-    function findShapesByName(names) {
-	return d3.selectAll("#map svg g path")
-	    .filter(function(d, i) {
-		return names.has(d.layer.name() + "/" + d.id);
-	    });
-    },
-    paint.redrawAll
-);
-
-paint.addClickHandler(function(id, layer) {
-    var tbody = layer.resultsTable.tbody().node();
-
-    layer.resultsTable.rows().each(function(d, i) {
-	var row = d3.select(this),
-	    chosen = row.datum()[0] === id;
-
-	if (chosen) {
-	    tbody.scrollTop = this.offsetTop - 
-		// Fudge factor found by experimentation, appears to work at different zoom levels.
-		// I don't know why this difference is here.
-		(2.7 * this.offsetHeight);
-	}
-
-	row.classed("selected", chosen);
-    });
-
-    layer.resultsTable.dialogue().show();
-});
-
-paint.addHoverHandler(function(id, layer) {
-    layer.resultsTable.setExtraRow(
-	layer.resultsTable.rows().filter(function(d, i) {
-	    return d3.select(this)
-		.datum()[0] === id;
-	})
-    );
-});
-
-require("./query-string.js")(wikiStore, title);
--- tile-layers.js
+++ tile-layers.js
@@ -10,75 +10,50 @@ var leaflet = require("leaflet"),
 	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }),
 
-    Stamen_TonerBackground = leaflet.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.png', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20
-    }),
-
-    Esri_WorldShadedRelief = leaflet.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri',
-	maxZoom: 13
-    }),
-
-    Esri_WorldImagery = leaflet.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    //http://test-tiles.0d9303a4.cdn.memsites.com/Total%20Heat%20Density/Z{z}/{y}/{x}.png
+    nationalHeatMap = leaflet.tileLayer('/heat-map-cdn/Total%20Heat%20Density/Z{z}/{y}/{x}.png', {
+	attribution: '<a href="http://tools.decc.gov.uk/nationalheatmap/">English National Heat Map</a>,',
+	minZoom: 2,
+	maxZoom: 17
     });
-
-//http://test-tiles.0d9303a4.cdn.memsites.com/Total%20Heat%20Density/Z{z}/{y}/{x}.png
-var nationalHeatMap = leaflet.tileLayer('/heat-map-cdn/Total%20Heat%20Density/Z{z}/{y}/{x}.png', {
-    attribution: '<a href="http://tools.decc.gov.uk/nationalheatmap/">English National Heat Map</a>,',
-    minZoom: 2,
-    maxZoom: 17
-});
 nationalHeatMap.options.zIndex = 1;
+
+var setupPixelHover = function(tileLayer) {
+    var colourChanged = callbackFactory();
+    tileLayer.colourChanged = colourChanged.add;
+
+    if (tileLayer.legend !== undefined) {
+
+	tileLayer.on("tileload", function(e) {
+	    var cache;
+
+	    d3.select(e.tile)
+		.on("mousemove", function() {
+		    if (cache === undefined) {
+			cache = document.createElement("canvas").getContext("2d");
+			cache.drawImage(this, 0, 0);
+		    }
+
+		    var rect = this.getBoundingClientRect(),
+			x = d3.event.offsetX ? d3.event.offsetX : d3.event.clientX - rect.left,
+			y = d3.event.offsetY ? d3.event.offsetY : d3.event.clientY - rect.top,
+			colourData = cache.getImageData(x, y, 1, 1).data;
+
+		    if (colourData[3] > 0) {
+			colourChanged(d3.rgb(colourData[0], colourData[1], colourData[2]));
+		    }
+		});
+	});
+    }
+};
 
 module.exports = function(map, errors) {
     nationalHeatMap.legend = require("./heat-map-legend.js")(map, errors);
-
-    var overlays = d3.map({
-	"English National Heat Map" : nationalHeatMap
-    });
-
-    overlays.values().forEach(function(tileLayer) {
-	var colourChanged = callbackFactory();
-	tileLayer.colourChanged = colourChanged.add;
-
-	if (tileLayer.legend !== undefined) {
-
-	    tileLayer.on("tileload", function(e) {
-		var cache;
-
-		d3.select(e.tile)
-		    .on("mousemove", function() {
-			if (cache === undefined) {
-			    cache = document.createElement("canvas").getContext("2d");
-			    cache.drawImage(this, 0, 0);
-			}
-
-			var rect = this.getBoundingClientRect(),
-			    x = d3.event.offsetX ? d3.event.offsetX : d3.event.clientX - rect.left,
-			    y = d3.event.offsetY ? d3.event.offsetY : d3.event.clientY - rect.top,
-			    colourData = cache.getImageData(x, y, 1, 1).data;
-
-			if (colourData[3] > 0) {
-			    colourChanged(d3.rgb(colourData[0], colourData[1], colourData[2]));
-			}
-		    });
-	    });
-	}
-    });
+    setupPixelHover(nationalHeatMap);
 
     return {
-	base: d3.map({
-	    "Open Street Map" : osmLayer,
-	    "Stamen Toner" : Stamen_TonerBackground,
-	    "ESRI Relief": Esri_WorldShadedRelief,
-	    "ESRI World Imagery" : Esri_WorldImagery
-	}),
-	overlays: overlays,
-	defaultBaseLayer: osmLayer
+	base: osmLayer,
+	overlay: nationalHeatMap
     };
 };
 
